1ed87ad641ac1c58861b9d9e440f6ee5fed08046
Per Wille <wille@napoleon.dk>
2014-08-27 16:32:33 +0200

    vmtools.sh fix providername

--- script/vmtools.sh
+++ script/vmtools.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -eux
 
-if [ $PACKER_BUILDER_TYPE == 'vmware' ]; then
+if [ $PACKER_BUILDER_TYPE == 'vmware-iso' ]; then
     echo "Installing VMware Tools"
-    apt-get install -y linux-headers-$(uname -r) build-essential perl
+    apt-get install -y linux-headers-$(uname -r) build-essential perl gcc
 
     cd /tmp
     mkdir -p /mnt/cdrom
@@ -15,7 +15,7 @@ if [ $PACKER_BUILDER_TYPE == 'vmware' ]; then
 
     apt-get -y remove linux-headers-$(uname -r) build-essential perl
     apt-get -y autoremove
-elif [ $PACKER_BUILDER_TYPE == 'virtualbox' ]; then
+elif [ $PACKER_BUILDER_TYPE == 'virtualbox-iso' ]; then
     echo "Installing VirtualBox guest additions"
 
     apt-get install -y linux-headers-$(uname -r) build-essential perl
