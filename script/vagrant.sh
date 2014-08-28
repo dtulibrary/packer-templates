fba31e83daba52adf5630c10c638ebee20b7580a
Lasse Aagren <laag@dtic.dtu.dk>
2014-08-28 20:39:41 +0200

    * packer first log in as root/vagrant instead vagrant/vagrant * use build-in packer webserver to preseed file * bundle preseed file * use ftp.dk.debian.org hosted iso image instead of local hosted

--- script/vagrant.sh
+++ script/vagrant.sh
@@ -21,4 +21,8 @@ chmod 700 /home/vagrant/.ssh
 cd /home/vagrant/.ssh
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
 chmod 600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant /home/vagrant/.ssh
+chown -R vagrant:vagrant /home/vagrant/.ssh
+
+#fix stuff
+sed -i '/UsePAM/aUseDNS no' /etc/ssh/sshd_config
+sed -i '/env_reset/aDefaults        env_keep += "SSH_AUTH_SOCK"' /etc/sudoers;
--- script/vmtools.sh
+++ script/vmtools.sh
@@ -6,10 +6,10 @@ if [ $PACKER_BUILDER_TYPE == 'vmware-iso' ]; then
 
     cd /tmp
     mkdir -p /mnt/cdrom
-    mount -o loop /home/vagrant/linux.iso /mnt/cdrom
+    mount -o loop /root/linux.iso /mnt/cdrom
     tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
     /tmp/vmware-tools-distrib/vmware-install.pl -d
-    rm /home/vagrant/linux.iso
+    rm /root/linux.iso
     umount /mnt/cdrom
     rmdir /mnt/cdrom
 
@@ -21,9 +21,9 @@ elif [ $PACKER_BUILDER_TYPE == 'virtualbox-iso' ]; then
     apt-get install -y linux-headers-$(uname -r) build-essential perl
     apt-get install -y dkms
 
-    VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
-    mount -o loop /home/vagrant/VBoxGuestAdditions_${VBOX_VERSION}.iso /mnt
+    VBOX_VERSION=$(cat /root/.vbox_version)
+    mount -o loop /root/VBoxGuestAdditions_${VBOX_VERSION}.iso /mnt
     sh /mnt/VBoxLinuxAdditions.run --nox11
     umount /mnt
-    rm /home/vagrant/VBoxGuestAdditions_${VBOX_VERSION}.iso
+    rm /root/VBoxGuestAdditions_${VBOX_VERSION}.iso
 fi
