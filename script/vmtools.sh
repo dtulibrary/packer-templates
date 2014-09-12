c74c856961cbab356dc62544de3dcaf8c253ee92
Per Wille <wille@dtic.dtu.dk>
2014-09-12 10:56:10 +0200

    Fix VMware guest tools issues

--- script/vmtools.sh
+++ script/vmtools.sh
@@ -5,11 +5,12 @@ if [ $PACKER_BUILDER_TYPE == 'vmware-iso' ]; then
     apt-get install -y linux-headers-$(uname -r) build-essential perl gcc fuse
 
     cd /tmp
+    modprobe loop
     mkdir -p /mnt/cdrom
-    mount -o loop /root/linux.iso /mnt/cdrom
+    mount -o loop /var/tmp/linux.iso /mnt/cdrom
     tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
     /tmp/vmware-tools-distrib/vmware-install.pl -d
-    rm /root/linux.iso
+    rm /var/tmp/linux.iso
     umount /mnt/cdrom
     rmdir /mnt/cdrom
 
