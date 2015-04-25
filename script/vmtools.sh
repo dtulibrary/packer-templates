bdd6c21e91a23bb65333455beae322b26b33b508
Alvaro Miranda <kikitux@gmail.com>
2015-04-25 13:30:09 +1200

    use open-vm-tools

--- script/vmtools.sh
+++ script/vmtools.sh
@@ -2,17 +2,12 @@
 
 if [ $PACKER_BUILDER_TYPE == 'vmware-iso' ]; then
     echo "Installing VMware Tools"
-    apt-get install -y linux-headers-$(uname -r) build-essential perl gcc fuse
 
-    cd /tmp
-    modprobe loop
-    mkdir -p /mnt/cdrom
-    mount -o loop /var/tmp/linux.iso /mnt/cdrom
-    tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
-    /tmp/vmware-tools-distrib/vmware-install.pl -d
-    rm /var/tmp/linux.iso
-    umount /mnt/cdrom
-    rmdir /mnt/cdrom
+    mkdir -p /mnt/hgfs
+    apt-get update
+    apt-get install -y open-vm-tools module-assistant linux-headers-amd64 linux-image-amd64 open-vm-tools-dkms
+    module-assistant prepare
+    module-assistant --text-mode -f get open-vm-tools-dkms
 
     apt-get -y remove linux-headers-$(uname -r) build-essential perl
     apt-get -y autoremove
